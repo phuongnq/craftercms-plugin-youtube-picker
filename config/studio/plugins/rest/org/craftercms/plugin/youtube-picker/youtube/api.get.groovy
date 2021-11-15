6a02fe65c26ee3d99e2d7d5899674c7cfad48245
Phil Nguyen <nquangphuong@gmail.com>
2021-11-15 18:59:00 +0700

    Copy plugin org.craftercms.plugin.youtube-picker 1.0.4

--- config/studio/plugins/rest/org/craftercms/plugin/youtube-picker/youtube/api.get.groovy
+++ config/studio/plugins/rest/org/craftercms/plugin/youtube-picker/youtube/api.get.groovy
@@ -18,7 +18,7 @@ if (StringUtils.isEmpty(keyword)) {
 
 def configurationService = applicationContext["configurationService"]
 def textEncryptor = applicationContext["crafter.textEncryptor"]
-def config = configurationService.legacyGetConfiguration(site, "site-config.xml");
+def config = configurationService.legacyGetConfiguration(site, "site-config.xml")
 
 if (config.youtubePicker != null && config.youtubePicker.apiKey != null) {
    def apiKeyEncrypt = config.youtubePicker.apiKey.trim()
---
+++ config/studio/plugins/rest/org/craftercms/plugin/youtube-picker/youtube/key_exists.groovy
@@ -0,0 +1,15 @@
+def result = [:]
+
+def site = request.getParameter("siteId")
+if (StringUtils.isEmpty(site)) {
+   result.code = 400;
+   result.message = "Invalid siteId"
+   return result
+}
+
+def configurationService = applicationContext["configurationService"]
+def config = configurationService.legacyGetConfiguration(site, "site-config.xml")
+
+result.exists = config.youtubePicker != null && config.youtubePicker.apiKey != null
+
+return result
